16f2c20124381e6167d6daa2f783b728702e6639
ArturSizov <Artur.Sizov@outlook.com>
2024-07-05 11:23:27 +0300

    Models added

---
+++ Reminder/DataAccessLayer/DAO/UserDAO.cs
@@ -0,0 +1,16 @@
+﻿namespace Reminder.DataAccessLayer.DAO
+{
+    /// <summary>
+    /// User Data Access Object
+    /// </summary>
+    public class UserDAO
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? Position { get; set; }
+        public string? Avatar { get; set; }
+        public DateTime? Birthday { get; set; }
+    }
+}
---
+++ Reminder/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using SDK.ViewModels.ViewModels;
+
+namespace Reminder.Models
+{
+    public class User : ViewModelBase
+    {
+        #region Private property
+        private int? _id;
+        private string? _name;
+        private string? _lastName;
+        private string? _middleName;
+        private string? _position;
+        private string? _avatar;
+        private DateTime? _birthday = new DateTime(2000, 1, 10);
+        #endregion
+
+        #region Public property
+        public int? Id { get => _id; set => SetProperty(ref _id, value); }
+        public string? Name { get => _name; set => SetProperty(ref _name, value); }
+        public string? LastName { get => _lastName; set => SetProperty(ref _lastName, value); }
+        public string? MiddleName { get => _middleName; set => SetProperty(ref _middleName, value); }
+        public string? Position { get => _position; set => SetProperty(ref _position, value); }
+        public string? Avatar { get => _avatar; set => SetProperty(ref _avatar, value); }
+        public DateTime? Birthday { get => _birthday; set => SetProperty(ref _birthday, value); }
+
+        #endregion
+    }
+}
