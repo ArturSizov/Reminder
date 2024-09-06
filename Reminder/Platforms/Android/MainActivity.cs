401e150f1edd6e97c28a55994984f19e49515e89
ArturSizov <Artur.Sizov@outlook.com>
2024-09-06 09:37:02 +0300

    Added to check SdkVersion. If above 33 will ask the user for notification permission

--- Reminder/Platforms/Android/MainActivity.cs
+++ Reminder/Platforms/Android/MainActivity.cs
@@ -1,11 +1,26 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
 
 namespace Reminder
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            if (!OperatingSystem.IsAndroidVersionAtLeast(33))
+                return;
+
+            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.PostNotifications) != Permission.Granted)
+            {
+                ActivityCompat.RequestPermissions(this, new[] { Manifest.Permission.PostNotifications }, 0);
+            }
+        }
     }
 }
